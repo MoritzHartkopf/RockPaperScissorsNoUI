7350758f15bfbfbcb402a9b6aa6fdaf3ccf7aada
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-04-28 13:28:03 +0200

    ex3 implemented

--- logic.js
+++ logic.js
@@ -10,17 +10,60 @@ function getComputerChoice () {
     return strings[randomIndex];
 }
 
-const random = getComputerChoice(); 
+// const random = getComputerChoice(); 
 
 function getPlayerChoice (){
     const playerChoice = prompt("Make your Choice (Rock, Paper or Scissors)")
     return playerChoice
 }
 
+
+
 function playRound(playerSelection, computerSelection){
-    return (
-        //if clause to check if you won
-        if 
-    )
+   
+
+if (playerSelection == "Rock" && computerSelection == "Scissors")
+{
+    return "You Win! Rock beats Scissors" 
+} 
+else if (playerSelection == "Rock" && computerSelection == "Rock")
+{
+    return "Rock and Rock is a tie"
+}
+else if (playerSelection == "Rock" && computerSelection == "Paper")
+{
+    return "You Lose! Paper beats rock"
 }
 
+else if (playerSelection == "Scissors" && computerSelection == "Scissors")
+{
+    return "Scissors and Scissors is a tie" 
+} 
+else if (playerSelection == "Scissors" && computerSelection == "Rock")
+{
+    return "You Lose! Rock beats Scissors"
+}
+else if (playerSelection == "Scissors" && computerSelection == "Paper")
+{
+    return "You Win! Scissors beats Paper"
+}
+
+else if (playerSelection == "Paper" && computerSelection == "Paper")
+{
+    return "Paper and Paper is a tie" 
+} 
+else if (playerSelection == "Paper" && computerSelection == "Rock")
+{
+    return "You Win! Paper beats Rock"
+}
+else if (playerSelection == "Paper" && computerSelection == "Scissors")
+{
+    return "You Lose! Scissors beats Paper"
+}
+
+
+}
+
+const selection = getPlayerChoice();
+const playerSelection = selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
+const computerSelection = getComputerChoice();
