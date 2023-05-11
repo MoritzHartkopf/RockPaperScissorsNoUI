543f15196c761bd68326c7ab3b73a71a75957cbb
MoritzHartkopf <106618531+MoritzHartkopf@users.noreply.github.com>
2023-05-11 19:47:59 +0200

    added a functional UI

--- logic.js
+++ logic.js
@@ -17,17 +17,22 @@ function getPlayerChoice (){
     return playerChoice
 }
 
-
+//let that keeps the current score
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection){
    
 
 if (playerSelection == "Rock" && computerSelection == "Scissors")
 {
+    playerScore ++;
     return "You Win! Rock beats Scissors" 
 } 
 else if (playerSelection == "Rock" && computerSelection == "Rock")
 {
+    playerScore ++;
+    computerScore ++;
     return "Rock and Rock is a tie"
 }
 else if (playerSelection == "Rock" && computerSelection == "Paper")
@@ -37,34 +42,108 @@ else if (playerSelection == "Rock" && computerSelection == "Paper")
 
 else if (playerSelection == "Scissors" && computerSelection == "Scissors")
 {
+    playerScore ++;
+    computerScore ++;
     return "Scissors and Scissors is a tie" 
 } 
 else if (playerSelection == "Scissors" && computerSelection == "Rock")
 {
+    computerScore ++;
     return "You Lose! Rock beats Scissors"
 }
 else if (playerSelection == "Scissors" && computerSelection == "Paper")
 {
+    playerScore ++;
     return "You Win! Scissors beats Paper"
 }
 
 else if (playerSelection == "Paper" && computerSelection == "Paper")
 {
+    computerScore ++;
+    playerScore ++;
+
     return "Paper and Paper is a tie" 
 } 
 else if (playerSelection == "Paper" && computerSelection == "Rock")
 {
+    playerScore ++;
+
     return "You Win! Paper beats Rock"
 }
 else if (playerSelection == "Paper" && computerSelection == "Scissors")
 {
+    computerScore ++;
     return "You Lose! Scissors beats Paper"
 }
 }
 
 
 
+/*function game (playerSelection){
+  
+  const selection = getPlayerChoice();
+const playerSelection = selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
 
+const computerSelection = getComputerChoice();
+      
+    console.log( playRound(playerSelection, computerSelection))
+    }
+    */
+
+function gameRock()
+{
+    const playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+
+    const div = document.getElementById('resultsDiv');                     
+    div.textContent = (playRound(playerSelection, computerSelection))
+}
+
+function gamePaper()
+{
+    const playerSelection = "Paper";
+    const computerSelection = getComputerChoice();
+    const div = document.getElementById('resultsDiv');                     
+    div.textContent = (playRound(playerSelection, computerSelection))
+}
+function gameScissors()
+{
+    const playerSelection = "Scissors";
+    const computerSelection = getComputerChoice();
+    const div = document.getElementById('resultsDiv');                     
+    div.textContent = (playRound(playerSelection, computerSelection))
+}
+
+      //adding Event listeners to the buttons 
+    btn1.addEventListener('click', gameRock); 
+    btn1.addEventListener('click', printResult); 
+
+    btn2.addEventListener('click', gamePaper); 
+    btn2.addEventListener('click', printResult); 
+
+    btn3.addEventListener('click', gameScissors);
+    btn3.addEventListener('click', printResult); 
+ 
+
+    // functions that prints the current score, and announces the first who wins 5
+    function printResult(){
+const currentScoreDiv = document.getElementById("currentResults");
+currentScoreDiv.textContent = "your score is: " + playerScore+ " and the computers score is: " + computerScore;
+const winner = document.getElementById("winnerDiv") ;
+if (playerScore >= 5) {
+winner.textContent = "Congratulations, you are the first to reach 5 and won the game!"
+}
+else if (computerScore >= 5)
+{
+    winner.textContent = "Nice try, but the computer reached 5 first. :( "
+}
+
+}
+
+
+
+
+/* this one is for 5 rounds 
 function game (){
     for (let i = 0; i < 7; i ++ ){
         const selection = getPlayerChoice();
@@ -74,4 +153,5 @@ const computerSelection = getComputerChoice();
     console.log( playRound(playerSelection, computerSelection))
     }
 }
+*/
 
